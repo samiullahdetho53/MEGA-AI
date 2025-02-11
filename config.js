403db6f7eacae84e9f84b848c46d7cad8ed8aeb5
samiullahdetho53 <samiullahdetho53@gmail.com>
2025-02-11 15:03:51 +0500

    Update config.js
    Id

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '923142817763' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'KRhSULID#3yDY6rux_3eISTbDj9run9yqc_6SQVk3S4WGUme3UDg' ;  // put your session id here
 
 global.mods = []
 global.prems = []
